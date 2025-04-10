fc65489626d83248ad4c4280a1b529de526eaf81
秦子琛 <qinzichen@wudaima.com>
2025-04-10 19:49:53 +0800

    修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -30,21 +30,17 @@ const routes = [
     meta: { verify: true },
     children: [
       {
-        path: "",
-        redirect: "basic"
-      },
-      {
-        path: "/basic",
+        path: "basic",
         component: () => import("@/view/setting/basic.vue"),
         meta: { verify: true }
       },
       {
-        path: "/default-model",
+        path: "default-model",
         component: () => import("@/view/setting/default-model.vue"),
         meta: { verify: true }
       },
       {
-        path: "/model",
+        path: "model",
         component: () => import("@/view/setting/model.vue"),
         meta: { verify: true }
       }
