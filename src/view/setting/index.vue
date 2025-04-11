051ced968f784c7a9bca651921cf30d64200e590
秦子琛 <qinzichen@wudaima.com>
2025-04-11 11:26:46 +0800

    修改返回

--- src/view/setting/index.vue
+++ src/view/setting/index.vue
@@ -2,7 +2,7 @@
   <div class="setting-container">
     <!-- Top Navigation Bar -->
     <div class="top-bar">
-      <a-button type="text" @click="$router.back()">
+      <a-button type="text" @click="back">
         <template #icon>
           <arrow-left-outlined />
         </template>
@@ -25,17 +25,17 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import { ArrowLeftOutlined } from '@ant-design/icons-vue'
 import SettingMenu from './MenuSide.vue'
 
-export default {
-  name: 'SettingPage',
-  components: {
-    ArrowLeftOutlined,
-    SettingMenu
-  }
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+const back = () => {
+  router.push('/')
 }
+
 </script>
 
 <style scoped>
